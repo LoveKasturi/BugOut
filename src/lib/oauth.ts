3420c1b2663fcff37c8ccf25c4dc48aa8546a819
LoveKasturi <runnergamings@gmail.com>
2025-01-13 20:21:08 +0530

    fixing oauth error

--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -26,7 +26,7 @@ export async function signUpWithGoogle() {
   
 	const redirectUrl = await account.createOAuth2Token(
 		OAuthProvider.Google,
-		`${origin}/oauth`,
+		`${process.env.NEXT_PUBLIC_APP_URL}/oauth`,
 		`${origin}/sign-up`,
 	);
 
