ee561dd55ab6dfc0c96d0ad8118ce8cba0f07828
LoveKasturi <runnergamings@gmail.com>
2025-01-13 19:53:37 +0530

    deployment

---
+++ src/features/auth/api/use-login.ts
@@ -0,0 +1,32 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType } from "hono";
+import { InferResponseType } from "hono";
+import {client} from "@/lib/rpc"
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+
+type ResponseType = InferResponseType<(typeof client.api.auth.login)["$post"]>;
+type RequestType = InferRequestType<(typeof client.api.auth.login)["$post"]>;
+
+export const useLogin = () => {
+  const router = useRouter();
+  const queryClient = useQueryClient();
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async ({ json }) => {
+      const response = await client.api.auth.login["$post"]({ json });
+      if (!response.ok) {
+        throw new Error("Failed to register");
+      }
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast.success("Logged In");
+      router.refresh();
+      queryClient.invalidateQueries({ queryKey: ["current"] });
+    },
+    onError: () => {
+      toast.error("Failed to login");
+    },
+  });
+  return mutation;
+};
